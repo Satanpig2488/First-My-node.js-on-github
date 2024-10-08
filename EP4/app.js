8132a2462e109d0a5c335137ff1b9d4c68123446
Satanpig2488 <satanpig@gmail.com>
2024-10-08 13:29:24 +0700

    cga

--- EP4/app.js
+++ EP4/app.js
@@ -2,13 +2,16 @@ const express = require('express')
 const chalk = require('chalk')
 const debug = require('debug')('app')
 const morgan = require('morgan')
+const path = require('path')
 const app = express()
 const port = 3001
 app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,"/public/")))
 app.get("/",(req,res)=>{
     res.send("Hello Borntodev ... . . .Togithub")
 })
 app.listen(port,()=>{
-    console.log(`Listening on ${chalk.green(port)}`)
+    //console.log(`Listening on ${chalk.green(port)}`)
     debug(`Listening on ${chalk.green(port)}`)
+    debug(`Dirname work on ${__dirname}`)
 })

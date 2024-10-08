d0afa62b43a147c3a48ba096c653947e9201b71f
Satanpig2488 <satanpig@gmail.com>
2024-10-08 13:05:44 +0700

    add all file to git project
    desc.

---
+++ EP4/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const chalk = require('chalk')
+const debug = require('debug')('app')
+const morgan = require('morgan')
+const app = express()
+const port = 3001
+app.use(morgan('combined'))
+app.get("/",(req,res)=>{
+    res.send("Hello Borntodev ... . . .")
+})
+app.listen(port,()=>{
+    console.log(`Listening on ${chalk.green(port)}`)
+    debug(`Listening on ${chalk.green(port)}`)
+})
